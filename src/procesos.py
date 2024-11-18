32e5aeb6f57f01d5e980139fa81238ec7a66221c
Mary Anne Crisan <mc312@inlumine.ual.es>
2024-11-18 13:06:01 +0100

    fix

--- src/procesos.py
+++ src/procesos.py
@@ -177,8 +177,10 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     sol=input("Do you want to overlap the signals? (y/n) ").lower()
     absolute_path=os.getcwd()
-    user_input = input("Do you want to use MST_16 or MST_32 for no overlap (16/32)? (Default is 16): ")
-    type_no_overlapping = int(user_input) if user_input.strip() else 16
+    
+    if(sol=="n"):
+        user_input = input("Do you want to use MST_16 or MST_32 for no overlap (16/32)? (Default is 16): ")
+        type_no_overlapping = int(user_input) if user_input.strip() else 16
     
     # Initialize the processor
     processor = Processing()
